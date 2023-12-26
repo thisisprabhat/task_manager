31342f8ba7f99afa42fb7d0366debd703d61a7a5
Prabhat Kumar <prabhat99998@gmail.com>
2023-12-26 17:46:56 +0530

    web render issue fixed

--- lib/presentation/widgets/loader.dart
+++ lib/presentation/widgets/loader.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -11,14 +12,21 @@ class Loader extends StatelessWidget {
     final Color loaderColor = Theme.of(context).colorScheme.primary;
     return Scaffold(
       body: Center(
-        child: ColorFiltered(
-          colorFilter: ColorFilter.mode(loaderColor, BlendMode.modulate),
-          child: Lottie.asset(
-            AppAssets.loaderAnimation,
-            height: 100,
-            width: 100,
-          ),
-        ),
+        // because flutter web doesn't support blend mode properly
+        child: kIsWeb
+            ? Lottie.asset(
+                AppAssets.loaderAnimation,
+                height: 100,
+                width: 100,
+              )
+            : ColorFiltered(
+                colorFilter: ColorFilter.mode(loaderColor, BlendMode.modulate),
+                child: Lottie.asset(
+                  AppAssets.loaderAnimation,
+                  height: 100,
+                  width: 100,
+                ),
+              ),
       ),
     );
   }
